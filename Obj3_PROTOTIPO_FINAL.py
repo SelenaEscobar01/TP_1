1bbbe35b3ff120edd34c95d6c93aca4c91f682dd
GustavoMaita <149437674+GustavoMaita@users.noreply.github.com>
2023-10-31 17:47:53 -0300

    Update Obj3_PROTOTIPO_FINAL.py

--- Obj3_PROTOTIPO_FINAL.py
+++ Obj3_PROTOTIPO_FINAL.py
@@ -98,22 +98,55 @@ def cifrado_cesar(cuadroUsuario,cuadroClave):
 
 
 def cifrado_atbash(cuadroUsuario):
-    
-    texto = cuadroUsuario.get()
 
-    original = "abcdefghijklmnñopqrstuvwxyz"
-    clave = "ZYXWVUTSRQPOÑNMLKJIHGFEDCBA"
-    cifrado = ""
-    lower = texto.lower()
-    for i in lower:
-        if i in original:
-                cifrado += clave[original.index(i)]
+    texto = cuadroUsuario.get()
+    
+    mensaje = texto
+    lista_1 = list(mensaje.lower())
+    cifrado = []
+    i = 0
+    for i in range(len(lista_1)): 
+        l= lista_1[i]
+        i +=1
+        if l.isnumeric():            
+            numero = 9 - int(l)
+            cifrado += str(numero)            
+        else: 
+            numero = ord(l)
+            valor = 122 - (numero-65)
+            cifrado += chr(valor)
+    if '\x9b' in cifrado:  
+        s = cifrado.index('\x9b')
+        cifrado[s]= " "
+    else:
+        cifrado
+        
+    cifrado = "".join(cifrado)
+    if texto.isupper():   
+        Str = cifrado.lower()
+    else:
+        Str = cifrado
+    Str = mostrar_texto(Str, texto)
+    
+    return Str
+
+
+def mostrar_texto(Str, texto):
+    j = 0
+    last=[]
+    for j in range(len(Str)):
+        if texto[j].isupper():
+            l = Str[j]
+            last.append(l.lower())
         else:
-                cifrado +=i
-    if texto.isupper():
-        messagebox.showinfo("El mensaje cifrado es:",cifrado.lower())
-    else: 
-        messagebox.showinfo("El mensaje cifrado es:",cifrado )
+            l = Str[j]
+            last.append(l)
+        j += 1
+ 
+        
+    mensaje_final = "".join(last)
+    
+    return(messagebox.showinfo("El mensaje cifrado es:",mensaje_final))
 
 
 ventana_principal()
